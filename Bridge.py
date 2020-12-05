6c1e0cd29f4f0df7f645c4832efb91726a715ca2
Ryan <artlessavian@gmail.com>
2020-12-04 22:21:37 -0800

    moving stuff, really bad example optimizer

--- Bridge.py
+++ Bridge.py
@@ -1,21 +1,60 @@
+import random
+import math
+
+def distance(point, other):
+    dx = point[0] - other[0]
+    dy = point[1] - other[1]
+    return math.sqrt(dx * dx + dy * dy)
+
 class Bridge:
-    def __init__(self, n_fixed = 2, n_main = 3, n_free = 3):
-
-        # probably could be renamed :)
-        self.main = [(0, 0) for i in range(n_main)]
-        self.free = [(0, 0) for i in range(n_free)]
+    def __init__(self, left = (-10, 0), right = (10, 0), other = [], n_main = 2, n_other = 3):
+        self.fixed = [left, right]
+        self.fixed.extend(other)
         
-        # [free] x [free U main U fixed]
-        self.edges = [[0 for j in range(n_fixed + n_main + n_free)] for i in range(n_free)]
+        self.n_main = n_main
+        # The first n_main are main joints.
+        self.free = [(0, 0) for i in range(n_main + n_other)]
+    
+        # TODO: Reduce wasted space
+        # [free] x [free U fixed]
+        self.edges = [[0 for j in range(len(self.fixed) + len(self.free))] for i in range(len(self.free))]
 
-        # TODO: theres some wasted space, edge[i][j] means the same as edge[j][i]
-        # and edge[i][i] is useless
-        # we'll ignore it for now :)
+        self.road_cost_per_length = 10 # Temporary
+
+    def objective_function(self):
+        strut_cost = 0
+
+        # Sum all struts.
+        for i in range(len(self.free)):
+            for j in range(i + 1, len(self.free)):
+                dist = distance(self.free[i], self.free[j])
+                strut_cost += dist * max(0, self.get_edge_to_free(i, j))
+
+            for j in range(len(self.fixed)):
+                dist = distance(self.free[i], self.fixed[j])
+                strut_cost += dist * max(0, self.get_edge_to_fixed(i, j))
+
+        # Sum road. (left to main, main to main, main to right)
+        road_length = 0
+        road_length += distance(self.fixed[0], self.free[0])
+        for i in range(self.n_main):
+            road_length += distance(self.free[i], self.free[i+1])
+        road_length += distance(self.free[self.n_main-1], self.fixed[1])
+
+        road_cost = road_length * self.road_cost_per_length
+
+        return strut_cost + road_cost
+
+    def equality_constraints(self):
+        return 0
+
+    def inequality_constraints(self):
+        return 0
+
+    # Vector Conversion
 
     def to_vector(self):
         vec = []
-        for point in self.main:
-            vec.extend(point)
         for point in self.free:
             vec.extend(point)
         for row in self.edges:
@@ -23,20 +62,65 @@ class Bridge:
         return vec
     
     def from_vector(self, vec):
-        # TODO: someone else get this lmao
-        pass
+        for i in range(len(self.free)):
+            self.free[i] = (vec[2 * i], vec[2 * i + 1])
+
+        start = 2 * len(self.free)
+        for i in range(len(self.edges)):
+            stride = len(self.fixed) + len(self.free)
+            offset = start + stride * i
+            self.edges[i] = vec[offset : offset + stride]
+
+    # Helpers to interpret the internal data.
 
     def get_edge_to_free(self, i, j):
         if (i > j):
             i, j = j, i
-        return self.edges[i][j]
-    
-    def get_edge_to_main(self, i, main):
-        return self.edges[i][main + len(self.free)]
+        return self.edges[i][j + len(self.fixed)]
 
     def get_edge_to_fixed(self, i, fixed):
-        return self.edges[i][fixed + len(self.free) + len(self.main)]
+        return self.edges[i][fixed]
+
+    def randomize(self):
+        for i in range(len(self.free)):
+            self.free[i] = (random.random() * 20 - 10, random.random() * 20 - 10)
+        for row in self.edges:
+            for j in range(len(row)):
+                row[j] = random.random()
+
+    # Helpers for humans.
+    def print_desmos_copypaste(self):
+        print()
+        print("Paste me!!")
+        print(str(self.fixed)[1:-1]) # code golfin
+        print(str(self.free[:self.n_main])[1:-1])
+        print(str(self.free[self.n_main:])[1:-1])
+        print()
+        
+        # Can't paste into desmos :(
+        # for i in range(len(self.free)):
+        #     for j in range(len(self.free)):
+        #         if (self.get_edge_to_free(i, j) < 0):
+        #             continue
+        #         if (self.free[i][0] >= self.free[j][0]):
+        #             continue
+
+        #         m = (self.free[i][1] - self.free[j][1]) / (self.free[i][0] - self.free[j][0])
+        #         b = self.free[i][1] - m * self.free[i][0]
+        #         print(f"y = {m}x + {b} {{{self.free[i][0]} < x < {self.free[j][0]}}}")
+
 
 if __name__ == "__main__":
     bridge = Bridge()
     print(bridge.to_vector())
+    print(bridge.objective_function())
+    
+    print()
+
+    bridge2 = Bridge()
+    bridge2.randomize()
+    bridge2.from_vector(list(range(100)))
+    print(bridge2.to_vector())
+    print(bridge2.objective_function())
+    
+    bridge2.print_desmos_copypaste()
--- Problem.py
+++ Problem.py
@@ -1,74 +1,59 @@
-import math
-
 from Bridge import Bridge
 
 class Problem:
-    def __init__(self, left, right, other = [], n_main = 3, n_free = 3):
-        self.bridge = Bridge(2 + len(other), n_main, n_free)
-        
-        self.fixed = [left, right]
-        self.fixed.extend(other)
+    def __init__(self, bridge = Bridge()):
+        self.bridge = bridge
 
-    def objective_function(self):
-        cost = 0
+    # def get_dimension(self):
+        # return len(self.bridge.to_vector())
 
-        for i in range(len(self.bridge.free)):
-            
-            for j in range(i + 1, len(self.bridge.free)):
-                dist = distance(self.bridge.free[i], self.bridge.free[j])
-                cost += dist * self.bridge.get_edge_to_free(i, j)
-
-            for j in range(len(self.bridge.main)):
-                dist = distance(self.bridge.free[i], self.bridge.main[j])
-                cost += dist * self.bridge.get_edge_to_main(i, j)
-
-            for j in range(len(self.fixed)):
-                dist = distance(self.bridge.free[i], self.fixed[j])
-                cost += dist * self.bridge.get_edge_to_fixed(i, j)
-
-        cost += distance(self.fixed[0], self.bridge.main[0])
-        for i in range(len(self.bridge.main) - 1):
-            cost += distance(self.bridge.main[i], self.bridge.main[i+1])
-        cost += distance(self.bridge.main[len(self.bridge.main)-1], self.fixed[1])
-
-        return cost
+    def objective_function(self, vec):
+        self.bridge.from_vector(vec)
+        return self.bridge.objective_function()
     
-    def equality_constraints(self):
-        return 0
+    def equality_constraints(self, vec):
+        self.bridge.from_vector(vec)
+        return self.bridge.equality_constraints()
 
-    def inequality_constraints(self):
-        return 0
+    def inequality_constraints(self, vec):
+        self.bridge.from_vector(vec)
+        return self.bridge.inequality_constraints()
 
-def distance(point, other):
-    dx = point[0] - other[0]
-    dy = point[1] - other[1]
-    return math.sqrt(dx * dx + dy * dy)
+# Move to optimizer!
+def calculate_gradient(vec, func, small_step = 1e-8):
+    gradient = []
+    old = func(vec)
+    for i in range(len(vec)):
+        vec[i] += small_step
+        new = func(vec)
+        vec[i] -= small_step
+        vec[i] -= small_step
+        old = func(vec)
+        vec[i] += small_step
+        
+        partial = (new - old) / small_step # / 2
+        gradient.append(partial)    
+    return gradient
+
 
 if __name__ == "__main__":
-    problem = Problem((-10, 0), (10, 0))
-    step = 0.0001
-
-    # This will work once someone writes from_vector()
-    gradient = []
-
-    old = problem.objective_function()
-    vec = problem.bridge.to_vector()
-
-    for i in range(len(vec)):
-        vec[i] += step
-        problem.bridge.from_vector(vec)
-        vec[i] -= step
-
-        new = (problem.objective_function())
-        partial = (new - old) / h
-        gradient.append(partial)
-
-    print(gradient)
-
-    # print(problem.objective_function())
     
-    # problem.bridge.main[0] = (-h, 0)
-    # print(problem.objective_function())
-
-
+    # Example usage?
+    bridge = Bridge()
+    bridge.randomize()
+    print(bridge.objective_function())
+    bridge.print_desmos_copypaste()
     
+    problem = Problem(bridge)
+    vec = bridge.to_vector()
+
+    for i in range(100):
+        grad = calculate_gradient(vec, problem.objective_function)
+        # print(grad)
+        vec = [vec[i] - grad[i] / 100 for i in range(len(vec))]
+        # print(vec)
+
+    # print([round(i*1000) / 1000 for i in vec])
+    bridge.from_vector(vec)
+    bridge.print_desmos_copypaste()
+    print(bridge.objective_function())
