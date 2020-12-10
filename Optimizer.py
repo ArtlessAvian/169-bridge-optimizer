e2413dae58d6ba78f637c9c987187b0c9cbed12e
Natalia Pinpin <nataliapinpin@natalias-mbp.attlocal.net>
2020-12-09 22:22:43 -0800

    Simplified penalty and step functions

--- Optimizer.py
+++ Optimizer.py
@@ -3,20 +3,15 @@ class ConstrainedOptimizer:
         # Need to update this
         self.lam = [[0 for _ in range(len(h[i]))] for i in range(len(h))]
         self.mu = [[0 for _ in range(len(g[i]))] for i in range(len(g))]
-        self.r = [0 for _ in range(len(h)+len(g))]
+        self.r = [1 for _ in range(len(h)+len(g))]  # something is wrong with this or the way I use self.r in the functions
 
     def penalty_function(self, vec, func, g, h):
         g_penalty,h_penalty = 0,0
 
         g_i = g(vec)
-        active = self.active_constraints()
         for i in range(len(g_i)):
-            if g_i[i] in active:
-                for j in range(len(vec)):
-                    g_penalty += self.mu[i][j] * g_i[i][j] + self.r[1]/2 * g[i][j]**2
-            else:
-                for j in range(len(vec)):
-                    g_penalty += self.r[2]/2 * (max(0, g[i][j]))**2
+            for j in range(len(vec)):
+                g_penalty += self.mu[i][j] * g_i[i][j] + self.r[1]/2 * (max(0, g[i][j]))**2
 
         h_i = h(vec)
         for i in range(len(h_i)):
@@ -27,9 +22,6 @@ class ConstrainedOptimizer:
 
     def penalty_gradient(self, vec, func, g, h):
         pass
-
-    def active_constraints(self):
-        return []
 
     def step(self, vec, func, g, h, a=2):
         T = lambda x: self.penalty_function(x, func, g, h)
@@ -45,19 +37,9 @@ class ConstrainedOptimizer:
 
         # Update mu
         g_i = g(vec)
-        active = self.active_constraints()
         for i in range(len(g_i)):
-            if g_i[i] in active:
                 for j in range(len(self.mu[i])):
-                    self.mu[i][j] += self.r[1] * g_i[i][j]
-                    if self.mu[i][j] < 0:
-                        self.mu[i][j] = 0
-            else:
-                for j in range(len(self.mu[i])):
-                    if g_i[i][j] <= 0:
-                        self.mu[i][j] = 0
-                    else:
-                        self.mu[i][j] = self.r[2] * g_i[i][j]
+                    self.mu[i][j] += max(0, self.r[1] * g_i[i][j])
 
         # Update r
         for i in range(len(self.r)):
