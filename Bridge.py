cc4ad61f72ca94189ac931eea2114b89501a1ff9
Ryan <artlessavian@gmail.com>
2020-12-11 05:34:04 -0800

    THATS RIGHT I DID IT
    WHO DO YOU THINK YOU ARE I AM
    
    its 5:30 am send help

--- Bridge.py
+++ Bridge.py
@@ -1,10 +1,20 @@
 import random
 import math
+from numpy import linalg
+import numpy as np
 
 def distance(point, other):
     dx = point[0] - other[0]
     dy = point[1] - other[1]
     return math.sqrt(dx * dx + dy * dy)
+
+def normalizedDiff(to, fromm):
+    dist = distance(to, fromm)
+    if dist == 0:
+        return (0, 1) # tbh, idk
+    dx = to[0] - fromm[0]
+    dy = to[1] - fromm[1]
+    return (dx / dist, dy / dist)
 
 class Bridge:
     def __init__(self, n_main = 1, n_other = 1, left = (-10, 0), right = (10, 0)):
@@ -12,8 +22,8 @@ class Bridge:
         # the next n_main have extra weight.
         # the rest (n_other) have some weight.
         self.nodes = [left, right]
-        self.nodes.extend((i, 0) for i in range(n_main))
-        self.nodes.extend((i, 5) for i in range(n_other))
+        self.nodes.extend((3 * i / n_main, 0) for i in range(n_main))
+        self.nodes.extend((3 * i / n_other, 5) for i in range(n_other))
 
         self.n_main = n_main
 
@@ -60,6 +70,7 @@ class Bridge:
                     last_other_index = other_index
 
         self.edge_width = [1 for i in self.members]
+        self.coeff = None
 
         self.road_cost_per_length = 10
 
@@ -90,30 +101,62 @@ class Bridge:
         constraints.extend(self.inequality_min_length())        
         return constraints
 
+    def coeff_matrix(self, force_recalc = False):
+        if (self.coeff != None and not force_recalc):
+            return self.coeff
+        
+        # NOTE: its row major lol
+        self.coeff = np.zeros((2 * len(self.nodes), len(self.members) + 3))
+
+        # internal forces
+        self.coeff[0][len(self.members)] = 1 # pin's x
+        self.coeff[1][len(self.members) + 1] = 1 # pin's y
+        self.coeff[3][len(self.members) + 2] = 1 # roller's y
+        # i is the compression/tension
+        for i, (fromm, to) in enumerate(self.members):
+            dx, dy = normalizedDiff(self.nodes[to], self.nodes[fromm])
+            # solving for i
+            # 0 = ... + i * dx + ...
+            self.coeff[2*fromm][i] = dx
+            # 0 = ... + i * dy + ...
+            self.coeff[2*fromm+1][i] = dy
+            # repeat
+            self.coeff[2*to][i] = -dx
+            self.coeff[2*to+1][i] = -dy
+
+        # # external reactions
+        # # total x
+        # self.coeff[len(self.members)][len(self.members)] = 1 # pin's x
+        # # total y
+        # self.coeff[len(self.members)+1][len(self.members)+1] = 1 # pin's y
+        # self.coeff[len(self.members)+1][len(self.members)+2] = 1 # roller's y
+        # # total moment, counterclockwise positive
+        # # moment = distance(self.nodes[0], self.nodes[1])
+        # # moment *= (self.nodes[1][0] - self.nodes[0][0]) / distance
+        # moment = self.nodes[1][0] - self.nodes[0][0]
+        # self.coeff[len(self.members)+2][len(self.members)+2] = moment # roller's y with shenanigans
+
+        return self.coeff
+
     def inequality_max_stress(self):
         constraints = []
         
-        solved = [False for vertex in self.free]
-        frontier = [vertex == 0 for vertex in self.free]
-        tension = [None for vertex in self.edges] # negative for tension, positive for compression
+        thingies = np.zeros(len(bridge.members) + 3)
+        # Push down on all main nodes
+        for i in range(self.n_main):
+            thingies[2 * (2 + i) + 1] = 10
+        forces = linalg.solve(bridge.coeff_matrix(), thingies)
 
-        # while any(frontier):
-        #     current = frontier.index(True)
-        #     sum_forces = [0, -2]            
-        
-        # if not all(solved):
-        #     print("oh no!")
+        for force, width in zip(forces, self.edge_width):
+            constraints.append(self.edge_width - forces)
         
         return constraints
 
     def inequality_min_length(self):
         min_length = 0.1
         constraints = []
-        for i in range(len(self.free)):
-            for j in range(i, len(self.free)):
-                constraints.insert(distance(self.free[i], self.free[j]) - min_length)
-            for j in range(len(self.fixed)):
-                constraints.insert(distance(self.free[i], self.fixed[j]) - min_length)
+        for i, j in self.members:
+            constraints.append(distance(self.nodes[i], self.nodes[j]) - min_length)
         return constraints
 
     # Vector Conversion
@@ -156,25 +199,37 @@ class Bridge:
         print()
 
 if __name__ == "__main__":
-    # Test if always statically determinate
-    for main in range(1, 20):
-        for other in range(1, 20):
-            determ = Bridge(main, other)
-            assert(len(determ.members) + 3 == 2 * len(determ.nodes))
+    # # Test if always statically determinate
+    # for main in range(1, 20):
+    #     for other in range(1, 20):
+    #         determ = Bridge(main, other)
+    #         assert(len(determ.members) + 3 == 2 * len(determ.nodes))
+
+    # # Test if solvable on init
+    # for main in range(1, 20):
+    #     for other in range(1, 20):
+    #         inverse = Bridge(main, other)
+    #         linalg.inv(inverse.coeff_matrix())
 
     # Test that vectors work as intended
-    bridge_vec = Bridge(10,10)
-    bridge_vec.from_vector(list(range(1000)))
-    vec = bridge_vec.to_vector()
-    for i, j in zip(vec, vec[1:]):
-        assert(i+1 == j)
+    # bridge_vec = Bridge(10,10)
+    # bridge_vec.from_vector(list(range(1000)))
+    # vec = bridge_vec.to_vector()
+    # for i, j in zip(vec, vec[1:]):
+    #     assert(i+1 == j)
 
     # Manual sanity checks
-    bridge = Bridge(1, 5)
+    bridge = Bridge(1, 1)
     print("  Nodes:", str(bridge.nodes))
     print("Members:", str(bridge.members))
 
     print(" Vector:", bridge.to_vector())
     print("Objctve:", bridge.objective_function())
 
-    bridge.print_desmos_copypaste()
+    thingies = np.zeros(len(bridge.members) + 3)
+    # Push down on a main node.
+    # The sum of forces should then be positive.
+    thingies[5] = 10
+    print(linalg.solve(bridge.coeff_matrix(), thingies))
+
+    # bridge.print_desmos_copypaste()
